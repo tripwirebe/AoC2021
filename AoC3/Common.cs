c1e853785eef0588ae7fd8b84fdf78bea04bbf87
michel.brouckaert <michel.brouckaert@gmail.com>
2021-12-02 19:24:09 +0100

    updated with feedback of Imbafaceroll

--- AoC3/Common.cs
+++ AoC3/Common.cs
@@ -3,14 +3,16 @@ using System.IO;
 namespace AoC.Common
 {
     public static class Common
-{
+    {
         public static string ReadInput(string fileLocation)
         {
             string output = string.Empty;
             try
             {
-                var sr = new StreamReader(fileLocation);
-                output = sr.ReadToEnd();
+                using (var sr = new StreamReader(fileLocation))
+                {
+                    output = sr.ReadToEnd();
+                }
             }
             catch (System.Exception)
             {
@@ -19,5 +21,5 @@ namespace AoC.Common
             }
             return output;
         }
-}
+    }
 }
