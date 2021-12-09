0b2dad1f2499336da1e63ce3bf2b7198328fb483
michel.brouckaert <michel.brouckaert@gmail.com>
2021-12-09 23:29:43 +0100

    added comments

--- AoC7b/Program.cs
+++ AoC7b/Program.cs
@@ -12,17 +12,25 @@ namespace AoC7b
             int numDays = 256;
             string [] input = Common.ReadInput(@"Input\input.txt").Split(',',StringSplitOptions.RemoveEmptyEntries);
             long[]fishes = new long[9];
+            // initialize the fishes array
             foreach (var i in input)
             {
                 fishes[Int32.Parse(i)]++;
             }
+            
             for (int i = 0; i < numDays; i++)
             {
+                // cycle every day
+                // the fishes that are giving birth get separated
                 long givingBirth = fishes[0];
+                //shift the array to the left and overwrite value 0
                 Array.Copy(fishes,1,fishes,0,8);
+                //fishes = giving brith = new youngsters
                 fishes[8]=givingBirth;
+                //fishes that gave brith get added to the next cycle
                 fishes[6]=fishes[6]+givingBirth;
             }
+            // sum of the array is the total amount of fishes
             long sum = fishes.Sum();
             Console.WriteLine(sum);
         }
