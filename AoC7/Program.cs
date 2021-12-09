6008a7c0d040a8eaed2c684695583ba343e07de4
michel.brouckaert <michel.brouckaert@gmail.com>
2021-12-09 23:49:36 +0100

    my first solution to day 6 - a wast of memory we can call  it

---
+++ AoC7/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using AoC.Common;
+
+namespace AoC7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfDays = 80;
+            // string []input = "3,4,3,1,2".Split(',',StringSplitOptions.RemoveEmptyEntries);
+            string[]input = Common.ReadInput(@"input\input.txt").Split(',',StringSplitOptions.RemoveEmptyEntries);
+            List<LanterFish> school = new List<LanterFish>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                school.Add(new LanterFish(Int32.Parse(input[i])));
+            }
+            for (int i = 0; i < numberOfDays; i++)
+            {
+                school = Tick(school);
+                Console.WriteLine(school.Count);
+            }
+            long memory = GC.GetTotalMemory(true);
+            Console.WriteLine(memory);
+        }
+
+        private static void printSchool(List<LanterFish> school)
+        {
+            string output = string.Empty;
+            foreach (var fish in school)
+            {
+                output+=fish.Counter.ToString()+",";
+            }
+            Console.WriteLine(output);
+        }
+
+        private static List<LanterFish> Tick(List<LanterFish> school)
+        {
+            List<LanterFish> youngSchool = new List<LanterFish>();
+            foreach (var lanterfish in school)
+            {
+                lanterfish.Counter--;
+                if(lanterfish.Counter<0){
+                    youngSchool.Add(new LanterFish());
+                    lanterfish.Counter=6;
+                }
+            }
+            school.AddRange(youngSchool);
+            return school;
+        }
+    }
+    internal class LanterFish
+    {
+        internal int Counter{get;set;}
+        public LanterFish(){
+            this.Counter=8;
+        }
+        public LanterFish(int input){
+            this.Counter = input;
+        }
+
+    }
+}
