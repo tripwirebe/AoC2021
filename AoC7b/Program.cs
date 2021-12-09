f6b49bf13029e76110648547f2a484fe0344e91a
michel.brouckaert <michel.brouckaert@gmail.com>
2021-12-09 23:26:50 +0100

    AoC7 rewrote the solution... because this was way easier

---
+++ AoC7b/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using AoC.Common;
+
+namespace AoC7b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int numDays = 80;
+            int numDays = 256;
+            string [] input = Common.ReadInput(@"Input\input.txt").Split(',',StringSplitOptions.RemoveEmptyEntries);
+            long[]fishes = new long[9];
+            foreach (var i in input)
+            {
+                fishes[Int32.Parse(i)]++;
+            }
+            for (int i = 0; i < numDays; i++)
+            {
+                long givingBirth = fishes[0];
+                Array.Copy(fishes,1,fishes,0,8);
+                fishes[8]=givingBirth;
+                fishes[6]=fishes[6]+givingBirth;
+            }
+            long sum = fishes.Sum();
+            Console.WriteLine(sum);
+        }
+    }
+}
